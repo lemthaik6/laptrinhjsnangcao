159b5059069f0a5a8ba552b56028abdd6b81c6c3
lemthaik6 <lemthai1808@gmail.com>
2025-09-23 10:38:36 +0700

    Initial commit: add lesson4.html and lesson4.js

---
+++ lesson4.js
@@ -0,0 +1,18 @@
+// console.log(document);
+
+// //truy suat the element
+// // var a = document.getElementById("div1");
+// // a.innerText = "Đây là div1 được thay đổi bởi JS";
+// // console.log(document.querySelector("#div1")); 
+// // console.log(document.querySelector(".class1"));
+
+// var a = document.querySelectorAll("div");
+// for (var i = 0; i < a.length; i++) {
+//     var r = Math.round(Math.random() * 255);
+//     var g = Math.round(Math.random() * 255);
+//     var b = Math.round(Math.random() * 255);
+//     a[i].style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+// }
+
+var a = document.getElementById("div1");
+console.log(a.firstChild);
