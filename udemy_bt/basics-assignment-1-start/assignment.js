5ddbc05ae01dc7ec4f09b60a218ff05bb3acae3d
lemthaik6 <lemthai1808@gmail.com>
2025-09-27 12:13:58 +0700

    Đẩy toàn bộ code lên GitHub

---
+++ udemy_bt/basics-assignment-1-start/assignment.js
@@ -0,0 +1,17 @@
+// 1. Tạo biến userInput (giả lập nhập từ người dùng) và biến result chưa có giá trị
+let userInput = 7; // bạn có thể chọn số bất kỳ
+let result;
+
+// 2. Gán result = 18 + userInput
+result = 18 + userInput;
+
+// 3. Thay đổi result bằng các phép toán khác
+result = result - 5;      // Trừ đi 5
+result = result * 2;      // Nhân với 2
+result = result / 3;      // Chia cho 3
+
+// 4. Giá trị userInput không thay đổi
+
+// 5. Hiển thị kết quả bằng alert
+alert(result);
+alert(userInput);
